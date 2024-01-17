fc14c54bd19f79f261d3a97b985b5af0db67375f
Harsh Agnihotri <harshagnihotri90@gmail.com>
2024-01-17 22:45:26 +0530

    booking working

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const server_url = "http://localhost:5000";
+// export const server_url = "http://localhost:5000";
 // export const server_url = "https://f854-2405-201-4035-4859-9b9-615f-fb08-6a4e.ngrok-free.app";
-// export const server_url = "http://api.fairflyings.com";
+export const server_url = "https://api.fairflyings.com/";
